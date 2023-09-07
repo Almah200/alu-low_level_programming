f2188691c91dee985556facd438e179cbf88f421
Almarat <arnualmarat@gmail.com>
2023-09-07 16:32:00 +0300

    4puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * this code demonstrate how the function main prints 
- * astring using puts 
- * 
+ * this code demonstrate how the function main prints
+ * astring using puts
+ *
  * puts prints "Programming is like building a multilingual puzzle"
  * and adds new line automatically at the end
  */
