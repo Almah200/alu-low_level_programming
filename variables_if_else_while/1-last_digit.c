ce0a18e04cf404e9252d36988e43b21b22108d75
Almarat <arnualmarat@gmail.com>
2023-09-14 00:28:05 +0300

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,6 @@
 /* more headers goes there */
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
@@ -15,11 +14,11 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    /* your code goes there */
 
     if (n % 10 > 5)
     {
@@ -31,7 +30,7 @@ int main(void)
     }
     else
     {
-        printf("Last digit of %d is %d and is 0\n", n, n % 10);
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
     }
 
     return (0);
