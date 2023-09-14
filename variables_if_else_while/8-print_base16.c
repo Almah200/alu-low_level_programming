c3240841879c3aba72df0f19cf0a611226004698
Almarat <arnualmarat@gmail.com>
2023-09-14 23:12:57 +0300

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This is the main function where the program starts execution.
+ * It generates a random number, assigns it to 'n', and prints whether 'n' is
+ * positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    char hex_digit;
+
+    for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
+    {
+        putchar(hex_digit);
+    }
+
+    for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+    {
+        putchar(hex_digit);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
