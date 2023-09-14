f52dba84dd7832a9927192f9c958316cf43715fd
Almarat <arnualmarat@gmail.com>
2023-09-14 21:41:01 +0300

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This is the main function where the program starts execution.
+ * It generates a random number, assigns it to 'n', and prints whether 'n' is
+ * positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int i;
+
+    for (i = 0; i <= 9; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
