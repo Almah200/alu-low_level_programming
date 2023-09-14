9e0c9322672583ff1f98dcf7c857a54ee281458d
Almarat <arnualmarat@gmail.com>
2023-09-14 21:18:58 +0300

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This is the main function where the program starts execution.
+ * It prints combinations of lowercase and uppercase alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char salphabet;
+    char calphabet;
+
+    for (salphabet = 'a'; salphabet <= 'z'; salphabet++) {
+        putchar(salphabet);
+    }
+
+    for (calphabet = 'A'; calphabet <= 'Z'; calphabet++) {
+        putchar(calphabet);
+    }
+
+    putchar('\n');
+    return (0);
+}
