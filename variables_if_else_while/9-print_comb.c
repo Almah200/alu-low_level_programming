a2185565aaa324b40ce639a414f1743c3da8f87a
Almarat <arnualmarat@gmail.com>
2023-09-16 22:10:11 +0300

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,21 +8,19 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    int a;
-    
-    for (a = 0; a < 10; a++)
-    {
-	    putchar(a + '0');
-	    if (a < 9)
-	    {
-		    putchar(',');
-		    putchar(' ');
-	    }
-    }
-    putchar('\n');
-    return (0);
+	int a;
 
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
