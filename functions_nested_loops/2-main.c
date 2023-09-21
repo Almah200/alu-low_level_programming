a94d80f23edeeb707ac725651973417094fa36be
Almarat <arnualmarat@gmail.com>
2023-09-21 17:06:17 +0300

    1

--- functions_nested_loops/2-main.c
+++ functions_nested_loops/2-main.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - check the code.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     print_alphabet_x10();
--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
 void print_alphabet_x10(void)
 {
+	
 	int i;
 	char c;
 
 	for (i = 0; i < 10; i++)
     {
-        for (c = 'a'; c <= 'z'; c++)
+        
+	    for (c = 'a'; c <= 'z'; c++)
 	{
             putchar(c);
         }
