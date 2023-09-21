ec731bb591e90642abf08299dc81af3ea976675b
Almarat <arnualmarat@gmail.com>
2023-09-21 16:57:13 +0300

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,13 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_alphabet_x10 - func
- *
- * Return - 0
- */
 void print_alphabet_x10(void)
 {
-int b = 0;
-while (b < 10)
-{
-char a = 'a';
-while (a <= 'z')
-{
-_putchar(a);
-a++;
-}
-_putchar('\n');
-b++;
-}
+    for (int i = 0; i < 10; i++)
+    {
+        for (char c = 'a'; c <= 'z'; c++)
+	{
+            putchar(c);
+        }
+        putchar('\n');
+    }
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,6 +6,6 @@
 
 /* Function prototypes */
 void print_alphabet(void);
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
