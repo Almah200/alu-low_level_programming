710294bb7eeee1e0d2156bd50570a172adb20078
Almarat <arnualmarat@gmail.com>
2023-09-21 19:29:45 +0300

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -6,11 +6,18 @@
  *
  * Return: 1 if the character is an alphabet letter, 0 otherwise.
  */
+
 int _isalpha(int c)
 {
-    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+
+    if (c >= 65 && c <= 90)
     {
-        return 1;
+	    return (1);
+    } 
+
+    else if (c >= 97 && c <= 122)
+    {
+	    return (1);
     }
     else
     {
