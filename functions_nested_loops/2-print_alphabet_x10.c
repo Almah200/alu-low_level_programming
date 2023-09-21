3aade2c6878cfcfc7a2cf7edb0029648ce71feb7
Almarat <arnualmarat@gmail.com>
2023-09-21 17:16:01 +0300

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - check the code.
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
+
 {
+
     int i;
     char c;
 
     for (i = 0; i < 10; i++)
+    
     {
         for (c = 'a'; c <= 'z'; c++)
-        {
-            putchar(c);
+        
+	{
+        
+	    	putchar(c);
         }
         putchar('\n');
     }
