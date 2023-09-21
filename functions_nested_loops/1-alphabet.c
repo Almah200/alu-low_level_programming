3c5da55b79cd7f921d1e505e9c73422d9d4fa853
Almarat <arnualmarat@gmail.com>
2023-09-21 11:55:20 +0300

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main() {
+    for (char c = 'a'; c <= 'z'; c++) {
+        putchar(c);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
