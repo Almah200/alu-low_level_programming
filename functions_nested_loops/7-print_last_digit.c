4fdc62801b80dd11054a6a3e3d01a18eb9ada798
Almarat <arnualmarat@gmail.com>
2023-09-22 20:50:10 +0300

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last digit of a number
+ * @n: The number to be checked
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+    int lastDigit = n % 10;
+    
+    return lastDigit;
+}
