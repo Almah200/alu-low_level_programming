028fdc821effd7aee296b2fac98f7aba6bb1cc6c
Almarat <arnualmarat@gmail.com>
2023-09-22 21:31:44 +0300

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_buer - prints every minute of the day of Jack Baue
+ * jack_bauer - prints every minute of the day of Jack Baue
  * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
