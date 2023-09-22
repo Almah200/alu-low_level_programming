fe94a4316f0a97f9c2385af05f931794543307e8
Almarat <arnualmarat@gmail.com>
2023-09-22 20:33:39 +0300

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,15 +2,13 @@
 
 /**
  * _abs - prints the absolute of a number
- * @n: The number to be checked
- * Return: 1 for positive num, -1 for negative num or zero for
- * anything else
+ * @a: The number to be checked
+ * Return: n if positive num, * -1 for negative num
  */
 
 int _abs(int a)
 {
 	if (a < 0)
-	
 	{
 		a = (-1) * a;
 	}
