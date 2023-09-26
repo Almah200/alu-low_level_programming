2020df2d96e861a1433e230da2d73ddeebdbab29
Almarat <arnualmarat@gmail.com>
2023-09-26 15:03:32 +0300

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,7 @@
 
 /**
  *reset_to_98 - function to update variable
- *by using pointer
- * int n is an interger variable who value is updated
+ @c : An input character
  * Return: Nothing
  */
 void reset_to_98(int *n)
