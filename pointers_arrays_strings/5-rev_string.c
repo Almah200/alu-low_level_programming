4ef08db12a0ae95b6246f238a078ff43fb2e473c
Almarat <arnualmarat@gmail.com>
2023-09-27 19:59:30 +0300

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,28 +4,26 @@
 /**
  * rev_string - reverses a string
  * @s: Pointer to a string to be reversed
- * @begin_ptr: second Pointer
- * @end_ptr: thir Pointer
+ * @begin_ptr:Pointer
+ * @end_ptr:Pointer
  *
  */
-void rev_string(char* s)
+void rev_string(char *s)
 {
 	int length, i;
 	char *begin_ptr, *end_ptr, ch;
+
 	length = strlen(s);
 	begin_ptr = s;
 	end_ptr = s;
-	
 	for (i = 0; i < length - 1; i++)
 		end_ptr++;
 	for (i = 0; i < length / 2; i++)
 	{
 		/* swapping */
-		ch = *end_ptr;
-		
+		ch = *end_ptr;	
 		*end_ptr = *begin_ptr;
 		*begin_ptr = ch;
-		
 		/* update begin and end pointers*/
 		begin_ptr++;
 		end_ptr--;
