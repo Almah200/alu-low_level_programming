120473970a8032a9cbe46cc1bc59f373731cad11
Almarat <arnualmarat@gmail.com>
2023-09-28 20:51:27 +0300

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - Returns every character of a string.
+ * @str: Poniter
+ */
+void puts2(char *str)
+{
+	int length = strlen(str);
+	int i;
+
+	for (i =0; i < length; ++i)
+	{
+		printf("%c\n", str[i]);
+	}
+}
