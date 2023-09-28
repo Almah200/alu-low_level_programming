b44aff022e7767c77e8df67d2dbfa33585b9c072
Almarat <arnualmarat@gmail.com>
2023-09-28 21:43:41 +0300

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,20 @@
 #include <string.h>
 
 /**
- *strncpy - copy from *src and paste it in *dest
+ * *_strncpy - copy from *src and paste it in *dest
  *@src: Pointer
- *
- *
+ *@desr: second Pointer to the duffer
+ *@n - interger
+ *Return: returns Pointer to dest
  */
 char *_strcpy(char *dest, const char *src, size_t n)
 {
 	size_t i;
+	
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
