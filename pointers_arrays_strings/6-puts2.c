664b51ff3d27b5199264f494ab707dd671f28c4b
Almarat <arnualmarat@gmail.com>
2023-09-28 21:06:10 +0300

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int length = strlen(str);
+    int i;
 
-	while (str[i] != '\0')
-	{
-		putchar(str[i]);
-		putchar('\n');
-		i++;
-	}
+    for (i = 0; i < length; ++i)
+    {
+        putchar(str[i]);
+    }
+    putchar('\n');
 }
