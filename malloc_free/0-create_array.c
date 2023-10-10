b33918bf625f771778e5ac3e4e5cc0b0d1a291aa
Almarat <arnualmarat@gmail.com>
2023-10-10 22:46:50 +0300

    readme

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,30 +4,32 @@
 
 /**
  *create_array - Pointer to an array
- *
+ *@size: size of an array
+ *@c: char c
+ *create_array - Returns array elements
  */
 char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
 
-	if( size == 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
-	
+
 	array = (char *)malloc(size * sizeof(char));
-	
+
 	if (array == NULL)
-	
+
 	{
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
-	
+
 	return (array);
 }
