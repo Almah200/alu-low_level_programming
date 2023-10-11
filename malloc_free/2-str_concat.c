f3a9e215d6743b3e90870f043d97335c870f20c1
Almarat <arnualmarat@gmail.com>
2023-10-11 22:41:29 +0300

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ *str_concat - concatanates two strings
+ *
+ */
 char *str_concat(char *s1, char *s2)
 {
 	char *rtn;
 	int p, q = 0;
 
-	if (s1 == NULL) s1 = "";
+	if (s1 == NULL) {
+		s1 = "";
+	}
 
-	if (s2 == NULL)	s2 = "";
+	if (s2 == NULL)	{
+		s2 = "";
+	}
 
 	rtn = malloc(strlen(s1) + strlen(s2) + 1);
 
