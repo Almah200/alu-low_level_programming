f2ae667bac233114235d9693c7739f686f2f30ce
Almarat <arnualmarat@gmail.com>
2023-10-18 21:49:17 +0300

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,15 +9,28 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	{
-		printf("nill\n");
-	} 
-	else 
-	
-	{
-		printf("Name: %s\n", (*d).name);
-		printf("Age: %f\n", (*d).age);
-		printf("Owner: %s\n", (*d).owner);
-	}
+    if (d == NULL)
+    {
+        printf("nill\n");
+    }
+    else
+    {
+        if (d->name == NULL)
+        {
+            printf("Name: (nil)\n");
+        }
+        else
+        {
+            printf("Name: %s\n", d->name);
+        }
+        printf("Age: %f\n", d->age);
+        if (d->owner == NULL)
+        {
+            printf("Owner: (nil)\n");
+        }
+        else
+        {
+            printf("Owner: %s\n", d->owner);
+        }
+    }
 }
