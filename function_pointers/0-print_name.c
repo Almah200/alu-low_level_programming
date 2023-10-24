f0d28324b13f1bf9636e256025122973b529118b
Almarat <arnualmarat@gmail.com>
2023-10-24 20:47:04 +0300

    readme and 0 file

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,6 +4,8 @@
 /**
  * print_name - Function that writes a name by taking function pointers
  * Returns - Nothing
+ * @name: variable name
+ * @f: Function pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
