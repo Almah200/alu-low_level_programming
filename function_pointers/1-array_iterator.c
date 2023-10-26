b1ee715f85c704e8f8e02b4bfaa7a048fb6a1495
Almarat <arnualmarat@gmail.com>
2023-10-26 20:31:21 +0300

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,12 +4,12 @@
  * array_iterator - Function to call action
  * @array: Pointer to an array
  * @size: size of an array
- * (*action)(int) - Function pointer
+ * @action - Function pointer
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-	for (i = 0; i < size ; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
