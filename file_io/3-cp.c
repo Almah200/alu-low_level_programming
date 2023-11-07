506333517a44a3e864305c60a61fb40cf9273021
Almarat <arnualmarat@gmail.com>
2023-11-07 19:45:53 +0300

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stddef.h>
+#include <stdio.h>
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments passed to the program
