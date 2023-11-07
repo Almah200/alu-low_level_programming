b2d9e46d50f93b40032cae925da27ac1c93073ec
Almarat <arnualmarat@gmail.com>
2023-11-07 10:21:04 +0300

    0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,51 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
  * Reads a text file and prints it's date to the POSIX standard output.
  *
  * @param filename: The name of the text file to be read.
  * @param letters: The number of letters to read and print.
- * @return: The actual number of letters read and printed.
- *          Returns 0 if the file cannot be opened or read, or if write fails or does not write the expected amount of bytes.
+ * Return: The actual number of letters read and printed.
+ * Returns 0 if the file cannot be opened or read, or if write fails or does not write the expected amount of bytes.
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
- 
-    FILE *file = fopen(filename, "r");
- 
-    if (file == NULL) {
-        return 0;
-    }
- 
-    char *buffer = (char *) malloc(letters + 1);
-
-    if (buffer == NULL) {
-        fclose(file);
-        return 0;
-    }
- 
-    ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
- 
-    if (bytesRead == 0) {
-        fclose(file);
-        free(buffer);
-        return 0;
-    }
- 
-    ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
- 
-    if (bytesWritten != bytesRead) {
-        fclose(file);
-        free(buffer);
-        return 0;
-    }
- 
-    fclose(file);
-    free(buffer);
- 
-    return bytesRead;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int a, b;
+	char *buf;
+	if (!filaname)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	a = read(fd, buf, letters);
+	if (a < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[a] = '\0';
+	close(fd);
+	b = write(STDOUT_FILENO, buf, a);
+	if (b < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (b);
 }
